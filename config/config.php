2eee6ccc9f45d2d6c9dc4402c581889d7127a396
Jinwoo Lee <winwoo3840@gmail.com>
2026-04-15 23:25:16 +0900

    Delete config/config.php

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'db');
-define('DB_USER', 'festival_user');
-define('DB_PASS', 'festival_pass');
-define('DB_NAME', 'festival_db');
-
-define('API_KEY', 'f504acd875f34d5e901630a686c28dff2d2dd86df7b9a1b6f019889e135e4822');
